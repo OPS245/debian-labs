5465b288f40e8915e7ed49b9afc42b89cb8b6fd1
Brian Gray <graybri@gmail.com>
2023-09-13 01:11:55 -0400

    lab2 check fix

--- lab2-check.bash
+++ lab2-check.bash
@@ -145,9 +145,7 @@ then
   echo "Congratulations!" | tee -a $logfile
   echo | tee -a $logfile
   echo "You have successfully completed Lab 2." | tee -a $logfile
-  echo "1. Submit a screenshot of your entire desktop (including this window) to your course professor." | tee -a $logfile
-  echo "2. A copy of this script output has been created at $logfile. Submit this file along with your screenshot." | tee -a $logfile
-  echo "3. Also submit a copy of your backupVM.py script." | tee -a $logfile
+  echo "Follow the submission instructions of your Professor. " | tee -a $logfile
   echo
 else
   echo "Your Lab is not complete." | tee -a $logfile
