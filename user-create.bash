aba31c8c715640477ce21eaa862dc92aa3f21294
Brian Gray <graybri@gmail.com>
2023-09-23 11:56:42 -0400

    added files for lab4 scripting

--- user-create.bash
+++ user-create.bash
@@ -1,35 +1,20 @@
+#!/bin/bash
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-        "http://www.w3.org/TR/html4/loose.dtd">
+# /root/user-create.bash
+# Author:  Murray Saul
+# Purpose: To create a list of users
+# USAGE: /root/user-create.bash
 
-<html>
+numberUsers=-1
+until [ $numberUsers -gt 0 ]
+do
+  numberUsers=$(zenity --entry --text "how many users to create?")
+done
 
-<head>
-    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-    <title>Restricted access</title>
-    <style type="text/css">
-	 body { background-image: url("/bg.png"); }
-	 body { text-align: center; padding: 100px; }
-	 h1 { font-size: 50px; }
-     body { font: 20px Helvetica, sans-serif; color: #333; }
-     #article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-     a { color: #dc8100; text-decoration: none; }
-     a:hover { color: #333; text-decoration: none; }
-    </style>
+for ((x=1; x <= $numberUsers; x++))
+do
+  userName=$(zenity --entry --text "Enter user Name #$x:")
+  useradd -m $userName 2> /dev/null # throw away errors (already exists)
+done
 
-</head>
-<body>
-        <div id="article">
-	<img src="/seneca-logo-red.png" />
-    <div>
-        <p>ict.senecacollege.ca is now protected using VPN and access is restricted to Employees and Students only.</p>
-		<p>Please connect to VPN and then try again.</p>
-		<ul>
-                   <li>Students: <a href="https://students.senecacollege.ca/spaces/186/it-services/wiki/view/1024/vpn">StudentVPN</a></li>
-                   <li>Employees: <a href="https://employees.senecacollege.ca/spaces/77/it-services/wiki/view/3723/employee-vpn">SenecaVPN</a></li>
-		</ul>
-		<br>
-    </div>
-    </div>
-</body>
-</html>
+zenity --info --text "All users created!"
